13676ec9809ce8d077e5aeb0bc3281dc15f634a8
demetoir <wnsqlehlswk@naver.com>
2016-08-31 17:46:27 +0900

    기본틀 작성

---
+++ 문제 풀이 컴파일 솔루션/소스.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <limits>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+typedef long long LL;
+
+#define INF 1234567890
+#define si(a) scanf("%d",&(a))
+#define slf(a) scanf("%lf",&(a))
+#define sll(a) scanf("%lld",&(a))
+#define sc(a) scanf("%c",&(a))
+#define ss(a) scanf("%s",a)
+
+
+
+
+int main() {
+
+
+
+	return 0;
+}
+
+
+
